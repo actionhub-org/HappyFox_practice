e9b0924a9b6e8b8cda11aa44aca868939dcc3a7a
MD-dev-007 <deebakbalaji07@gmail.com>
2025-07-23 00:50:33 +0530

    Re-add frontend as normal directory

---
+++ frontend/src/App.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('/')
+      .then(res => setMessage(res.data.message))
+      .catch(err => setMessage('Error connecting to backend'));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Backend says: <strong>{message}</strong>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
