96c97170a91c4b23f583e89563e545d7733a8fc7
Akshayaa139 <akshlearn13@gmail.com>
2025-07-23 17:43:01 +0530

    Payment module added

---
+++ frontend/src/Cancel.js
@@ -0,0 +1,4 @@
+function Cancel() {
+  return <h1>❌ Payment canceled. Please try again.</h1>;
+}
+export default Cancel;
---
+++ frontend/src/Success.js
@@ -0,0 +1,4 @@
+function Success() {
+  return <h1>✅ Payment successful! Thank you!</h1>;
+}
+export default Success;
